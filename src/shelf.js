bb4874a706785dfb561faa043668d2b492873846
Jenna DiLullo <jenna.dilullo@gmail.com>
2020-05-07 16:23:21 -0600

    Finish listTitles section. Could use refactoring

--- src/shelf.js
+++ src/shelf.js
@@ -13,7 +13,11 @@ for (var i = 0; i < scifiShelf.length; i++) {
 }
 
 function listTitles(fantasyShelf) {
-
+  var firstTitle = fantasyShelf[0].title;
+  var secondTitle = fantasyShelf[1].title;
+  var thirdTitle = fantasyShelf[2].title;
+  var stringOfTitles = `${firstTitle}, ${secondTitle}, ${thirdTitle}`;
+    return stringOfTitles.toString();
 }
 
 
@@ -22,5 +26,5 @@ module.exports = {
   shelfBook: shelfBook,
   unShelfBook: unShelfBook,
   listTitles: listTitles,
-  // searchShelf: searchShelf
+  searchShelf: searchShelf
 };
