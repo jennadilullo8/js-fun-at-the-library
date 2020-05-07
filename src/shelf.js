a2cbc10268cb37815ff82470f49849206de35ada
Jenna DiLullo <jenna.dilullo@gmail.com>
2020-05-07 15:13:07 -0600

    Finish unShelfBook and wrote the function listTitles

--- src/shelf.js
+++ src/shelf.js
@@ -5,7 +5,14 @@ function shelfBook(book, scifiShelf) {
   }
 }
 
-function unShelfBook() {
+function unShelfBook(bookString, scifiShelf) {
+for (var i = 0; i < scifiShelf.length; i++) {
+  if (scifiShelf[i].title === 'Dune')
+    scifiShelf.splice(i, 1);
+  }
+}
+
+function listTitles(fantasyShelf) {
 
 }
 
@@ -14,6 +21,6 @@ function unShelfBook() {
 module.exports = {
   shelfBook: shelfBook,
   unShelfBook: unShelfBook,
-  // listTitles: listTitles,
+  listTitles: listTitles,
   // searchShelf: searchShelf
 };
