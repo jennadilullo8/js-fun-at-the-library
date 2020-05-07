f35ed247255982ae0c67d9a1a40086f972c0952e
Jenna DiLullo <jenna.dilullo@gmail.com>
2020-05-06 20:37:39 -0600

    Add createTitle, buildMainCharacter, and saveReview functions to book.js

--- src/book.js
+++ src/book.js
@@ -1,11 +1,27 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+var ghoulCharacter = {
+  name: 'Vassya',
+  age: 16,
+  pronouns: 'she/her'
+};
+
+function buildMainCharacter() {
+  return ghoulCharacter;
+}
+
+function saveReview(string, reviews) {
+  if (!reviews.includes(string)) {
+       reviews.push(string);
+  }
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
   // calculatePageCount: calculatePageCount,
   // writeBook: writeBook,
   // editBook: editBook
