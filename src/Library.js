9096bd958a5d7480d9b66bdb7523d9cdaafad1b0
Jenna DiLullo <jenna.dilullo@gmail.com>
2020-05-08 17:03:00 -0600

    Fix functionality of addBook function and almost complete with checkOutBook function

--- src/Library.js
+++ src/Library.js
@@ -11,15 +11,29 @@ function createLibrary(name) {
 }
 
 function addBook(libraryObj, bookTitle) {
-  if (bookTitle.title === "Dracula") {
+  if (bookTitle.genre === 'fantasy') {
     libraryObj.shelves.fantasy.push(bookTitle);
-  } else {
+  } else if (bookTitle.genre === 'nonFiction') {
     libraryObj.shelves.nonFiction.push(bookTitle);
-  };
+  } else {
+    libraryObj.shelves.fiction.push(bookTitle);
+  }
+}
+
+
+function checkoutBook(libraryObj, bookTitleString) {
+  if (libraryObj.shelves.fiction[0].title === bookTitleString) {
+      libraryObj.shelves.fiction.pop(bookTitleString);
+      return `You have now checked out ${bookTitleString} from the Denver Public Library`;
+      console.log(libraryObj.shelves.fiction[0].title);
+  } else if (libraryObj.shelves.title != bookTitleString){
+    //if the booktitle doesn't matche the booktitlestring then return
+    return `Sorry, there are currently no copies of the ${bookTitleString} available at the Denver Public Library`;
+  }
 }
 
 module.exports = {
   createLibrary: createLibrary,
   addBook: addBook,
-  // checkoutBook: checkoutBook
+  checkoutBook: checkoutBook
 };
