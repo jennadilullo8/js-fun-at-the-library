07303288788c6877bfc746237d3bbc6651bb6635
Jenna DiLullo <jenna.dilullo@gmail.com>
2020-05-08 07:52:02 -0600

    Complete createLibrary section by creating a function createLibrary with an object that has key:value pairs and a new shelf object within that object

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,17 @@
-
+function createLibrary(name) {
+  var libraryObj = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    },
+  };
+  return libraryObj;
+}
 
 module.exports = {
-  // createLibrary: createLibrary,
+  createLibrary: createLibrary,
   // addBook: addBook,
   // checkoutBook: checkoutBook
 };
